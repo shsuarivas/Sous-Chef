8409020285a92d2506338b5f66e43af3835b295f
shsuarivas <aer103@shsu.edu>
2026-02-07 13:21:20 -0600

    unfinished sidebar

--- Frontend/src/Homepage/Homepage.jsx
+++ Frontend/src/Homepage/Homepage.jsx
@@ -1,13 +1,18 @@
 import style from './Homepage.module.scss'
+import Sidebar from './Sidebar.jsx'
 import TitleBar from './TitleBar.jsx'
 import RecipeArea from './RecipeArea/RecipeArea.jsx'
 
 function Homepage() {
   return (
-    <>
-      <div className={style.title_div}>
+    <> 
+    <div className={style.title_div}>
         <TitleBar />
-      </div>
+    </div>
+
+    <div className={style.sidebar_div}>
+      <Sidebar />
+    </div>
 
       <div className={style.recipes_div}>
         <RecipeArea />
