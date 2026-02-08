fa555b197684e14a9091176b255e5e2c61020e24
Christopher Chamberlain <chambca@gmail.com>
2026-02-08 09:54:25 -0600

    Add simple homepage layout

--- Frontend/src/MainPage/pages/HomePage/HomePage.jsx
+++ Frontend/src/MainPage/pages/HomePage/HomePage.jsx
@@ -1,7 +1,25 @@
-export default function HomePage() {
+import styles from './HomePage.module.scss';
+
+function Recipe() {
     return (
         <>
-            Home
+            <div className={styles.recipe_div}>
+
+            </div>
+        </>
+    );
+}
+
+export default function HomePage() {
+
+    let recipes = [];
+    for (let i=0; i<50; i++) recipes.push(<Recipe />);
+
+    return (
+        <>
+            <div className={styles.main_div}>
+                {recipes}
+            </div>
         </>
     );
 };
