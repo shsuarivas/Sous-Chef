cb7f02c869f8c36d2e2a97db3b396cd20d491ab1
Christopher Chamberlain <chambca@gmail.com>
2026-02-10 10:13:31 -0600

    Add recipe categories

--- Frontend/src/MainPage/pages/HomePage/HomePage.jsx
+++ Frontend/src/MainPage/pages/HomePage/HomePage.jsx
@@ -10,15 +10,38 @@ function Recipe() {
     );
 }
 
-export default function HomePage() {
+function RecipeCategory({
+    categoryName,
+    recipes,
+}) {
+    return (
+        <>
+            <div className={styles.category_div} >
+                <span className={styles.category_title}>{categoryName}</span>
+                <div className={styles.category_grid}>
+                    {recipes}
+                </div>
+            </div>
+        </>
+    );
+}
 
-    let recipes = [];
-    for (let i=0; i<25; i++) recipes.push(<Recipe />);
+export default function HomePage() {
+    let recipes1 = [];
+    for (let i=0; i<5; i++) recipes1.push(<Recipe />);
+
+    let recipes2 = [];
+    for (let i=0; i<16; i++) recipes2.push(<Recipe />);
+
+    let recipes3 = [];
+    for (let i=0; i<8; i++) recipes3.push(<Recipe />);
 
     return (
         <>
             <div className={styles.main_div}>
-                {recipes}
+                <RecipeCategory categoryName="Soup" recipes={recipes1} />
+                <RecipeCategory categoryName="Pasta" recipes={recipes2} />
+                <RecipeCategory categoryName="Barbecue" recipes={recipes3} />
             </div>
         </>
     );
