f78dc66616bad239eadabc136e3c3bb469ba54d1
Christopher Chamberlain <chambca@gmail.com>
2026-02-10 10:35:57 -0600

    Initialize npm module for backend

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+const PORT = 8080;
+
+let app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Test!');
+});
+
+app.listen(8080, () => {
+    console.log(`Backend listening on port ${PORT}`);
+});
