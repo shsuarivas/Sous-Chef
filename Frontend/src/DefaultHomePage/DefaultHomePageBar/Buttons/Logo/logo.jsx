00544be13bde600c762e2cf79bd4aa439caee409
Bruce <bandit31@bandit.labs.overthewire.org>
2026-04-09 13:15:02 -0500

    Updated logo route

--- Frontend/src/DefaultHomePage/DefaultHomePageBar/Buttons/Logo/logo.jsx
+++ Frontend/src/DefaultHomePage/DefaultHomePageBar/Buttons/Logo/logo.jsx
@@ -1,10 +1,11 @@
 import logo from './Sous_Chef_Logo.png'
 import styles from './logo.module.scss'
+import { Link } from 'react-router-dom'
 
 export default function Logo() {
     return (
-        <a href = "/defaultHomePage" target="_blank" rel="noreferrer">
+        <Link to="/">
             <img src={logo} className={styles.logo} alt="byf logo" />
-        </a>
+        </Link>
     );
 };
