b5c80b9bf1b3bf6083272f1f7b8de3efbc674b58
shsuarivas <aer103@shsu.edu>
2026-04-10 00:03:58 -0500

    Gemini Live API Websocket Authentication and Authorization

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import cors from 'cors'
-
+import { GoogleGenAI } from '@google/genai';
+import 'dotenv/config';
 const PORT = 8080;
 
 let app = express();
 app.use(cors());
+app.use(express.json()); 
+
 
 app.get('/', (req, res) => {
     res.send('Test!');
@@ -12,4 +15,31 @@ app.get('/', (req, res) => {
 
 app.listen(8080, () => {
     console.log(`Backend listening on port ${PORT}`);
+});
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+app.get("/api/token", async (req, res) => {
+  try {
+    const expireTime = new Date(Date.now() + 30 * 60 * 1000).toISOString();
+    const token = await ai.authTokens.create({
+      config: {
+        uses: 1,
+        expireTime: new Date(Date.now() + 30 * 60 * 1000).toISOString(),
+        newSessionExpireTime: new Date(Date.now() + 5 * 60 * 1000).toISOString(),
+        liveConnectConstraints: {
+          model: "gemini-3.1-flash-live-preview",
+          config: {
+            responseModalities: ["AUDIO"],
+          }
+        },
+        httpOptions: { apiVersion: "v1alpha" },
+      }
+    });
+
+    console.log("Full token object:", JSON.stringify(token)); // ← add this
+    res.json({ token: token.name });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
