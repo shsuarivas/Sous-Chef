a17b1ce568e39e4c9ceb44d828096cf3939a8717
Bruce <bandit31@bandit.labs.overthewire.org>
2026-04-11 20:15:17 -0500

    Updated main page styling

--- Frontend/src/MainPage/pages/NotificationsPage/NotificationsPage.jsx
+++ Frontend/src/MainPage/pages/NotificationsPage/NotificationsPage.jsx
@@ -1,7 +1,19 @@
-export default function NotificationsPage() {
-    return (
-        <>
-            Notifications
-        </>
-    );
-};
+import styles from './NotificationsPage.module.scss';                                                       
+                                                                                                              
+  function Notification({ message }) {                                                                        
+      return (    
+          <div className={styles.notification_div}>                                                           
+              {message}
+          </div>
+      );
+  }
+
+  export default function NotificationsPage() {
+      return (
+          <div className={styles.main_div}>                                                                   
+              <Notification message="A new recipe was added!" />
+              <Notification message="A new recipe was added!" />                                              
+              <Notification message="A new recipe was added!" />
+          </div>                                                                                              
+      );
+  }                           
