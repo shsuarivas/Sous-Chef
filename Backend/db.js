84a9eea0eadd9926410df0ef235f3552dc4739f4
Gio <gio.a.martinez03@gmail.com>
2026-04-12 19:30:44 -0500

    fix: wire db.js connection to use DATABASE_URL environment variable

--- Backend/db.js
+++ Backend/db.js
@@ -1,12 +1,6 @@
 import pg from 'pg';
 const { Pool } = pg;
-
 const pool = new Pool({ 
-  //user: 'SousChef', // username of .env username
-  host: 'postgres', 
-  database: 'appdb', // database name of .env database
-  //password: '1234', // password of .env password
-  port: 5432,
+  connectionString: process.env.DATABASE_URL
 });
-
 export default pool;
