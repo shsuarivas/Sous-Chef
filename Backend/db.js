9035f73227340368303d49e3f52f9ecfb1b40af7
Bruce <bandit31@bandit.labs.overthewire.org>
2026-04-12 21:31:04 -0500

    Add recipe card display with images

--- Backend/db.js
+++ Backend/db.js
@@ -1,12 +1,8 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-const pool = new Pool({ 
-  //user: 'SousChef', // username of .env username
-  host: 'postgres', 
-  database: 'appdb', // database name of .env database
-  //password: '1234', // password of .env password
-  port: 5432,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
