b3007afbf9ac46ee6cecce030da9ed6985cda22c
Gio <gio.a.martinez03@gmail.com>
2026-04-12 21:42:23 -0500

    Feat: Added reset username, email & password functionality for users

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import authRouter from './src/routes/auth.js';
+import userRouter from './src/routes/user.js';
 
 const PORT = process.env.PORT || 8080;
 
@@ -13,6 +14,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authRouter);
+app.use('/user', userRouter);
 
 app.listen(PORT, () => {
     console.log(`Backend listening on port ${PORT}`);
