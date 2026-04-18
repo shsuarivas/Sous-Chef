5b610fcbbaaf9c33f12c9c41d61ac78c1dfbd327
Gio <gio.a.martinez03@gmail.com>
2026-04-17 22:48:39 -0500

    Added user rating logic and routes in the backend as well as the UI functionality in the front end. very bare.

--- Frontend/src/MainPage/pages/RecipePage/RecipePage.jsx
+++ Frontend/src/MainPage/pages/RecipePage/RecipePage.jsx
@@ -5,7 +5,9 @@ export default function RecipePage(){
 	// grab the recipe id from the URL
 	const {id} = useParams();
 	const [recipe,setRecipe] = useState(null);
-
+	const [ratingData, setRatingData] = useState({average: null, count: 0});
+	const user = JSON.parse(localStorage.getItem('user'));
+	const [userRating, setUserRating] = useState(0); //different from ratingData, userRating is the rating the user selects for a recipe
 	// fetch full recipe details whenever the id changes
 	useEffect(() => {
 		fetch(`${import.meta.env.VITE_API_URL}/recipes/${id}`)
@@ -13,6 +15,36 @@ export default function RecipePage(){
 			.then (data => setRecipe(data))
 			.catch(err => console.error('Failed to fetch recipe :(((', err));
 	},[id]);
+
+	useEffect(() => {
+		fetch(`${import.meta.env.VITE_API_URL}/recipes/${id}/ratings`)
+			.then (res => res.json())
+			.then (data => setRatingData(data))
+				.catch(err => console.error(' bruh this failed to fetch ratings dawg', err));
+	},[id]);
+
+	// UI and front end logic for users to actually rate recipes.
+	
+	
+	function submitRating(star){
+		if (!user) return; //cant do nun if user not logged in
+		setUserRating(star)
+		fetch(`${import.meta.env.VITE_API_URL}/recipes/${id}/ratings`, {
+			method: 'POST',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({ user_id: user.id, rating: star })
+		})
+		.then(res => res.json())
+		.then(() => {
+			//refresh the average after submitting new ratiing
+				fetch(`${import.meta.env.VITE_API_URL}/recipes/${id}/ratings`)
+					.then(res => res.json())
+					.then(data => setRatingData(data))
+					.catch(err => console.error('bruh this failed to fetch ratings dawg', err));
+		})
+		.catch(err => console.error('Failed to submit rating', err));
+	}
+
 
 	return(
 		<div>
@@ -22,6 +54,19 @@ export default function RecipePage(){
 			<img src={recipe.image_url} alt={recipe.recipe_name}/>
 			<h1>{recipe.recipe_name}</h1>
 			<p> Serves {recipe.servings} * {recipe.time_to_cook} min(s)</p>
+		{/* Star logic */}	
+			<div>
+					{[1,2,3,4,5].map(star =>  (
+						<span
+						key={star}
+						onClick={() =>  submitRating(star)}
+						style={{cursor: 'pointer', fontSize: '1.5rem', color: star <= userRating ? 'gold' : 'grey' }}
+						>
+						★
+						</span>
+					))}
+				<span> {ratingData.average ? `${ratingData.average}/5 (${ratingData.count} ratings)` : 'No ratings yet'}</span>
+			</div>
 
 			<h2>Ingredients</h2>
 			<ul style={{listStylePosition: 'inside'}}>
