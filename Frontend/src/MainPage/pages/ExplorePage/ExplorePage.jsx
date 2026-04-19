ec186f1892e3288ffef18caad6e9b557d295f0e6
Tyler Byars <tylerabyars@gmail.com>
2026-04-19 16:16:08 -0500

    recipe component passing logic

--- Frontend/src/MainPage/pages/ExplorePage/ExplorePage.jsx
+++ Frontend/src/MainPage/pages/ExplorePage/ExplorePage.jsx
@@ -2,15 +2,25 @@ import styles from './ExplorePage.module.scss'
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
- function Recipe() {
+//passing prop to a function
+
+
+ function Recipe({id, recipe_name, recipe_description, image_url, handleRecipeClick}) {
       return (
-          <div className={styles.recipe_div}>                                                                 
+          <div 
+            className={styles.recipe_div}
+            onClick= {() => handleClickRecipe(id)}
+          >       
+           <img src= " "/>
+
           </div>
       );                                                                                                      
   }               
  
 
 export default function ExplorePage() {
+    const [recipes, setRecipes] = useState([]);
+
       let recipes = [];
       for (let i = 0; i < 20; i++) recipes.push(<Recipe />);
                                                                                                               
