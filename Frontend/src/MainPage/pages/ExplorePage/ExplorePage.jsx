4e3982ce0fafffa248efaf9bdd0754bf567b730b
Tyler Byars <tylerabyars@gmail.com>
2026-04-19 20:07:02 -0500

    making a static explore page and deleted modules since we don't have data. lol.

--- Frontend/src/MainPage/pages/ExplorePage/ExplorePage.jsx
+++ Frontend/src/MainPage/pages/ExplorePage/ExplorePage.jsx
@@ -1,9 +1,5 @@
 import styles from './ExplorePage.module.scss'
-import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-//passing prop to a function
-
 
  function Recipe({id, recipe_name, recipe_description, image_url, handleRecipeClick}) {
       return (
@@ -19,14 +15,11 @@ import { useNavigate } from 'react-router-dom';
  
 
 export default function ExplorePage() {
-    const [recipes, setRecipes] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(0);
-    const [selectedTag, setSelectedTag] = useState(null);
-
     const navigate = useNavigate(); 
     
+    function handleRecipeClick(id) {
+        navigate(`/main/recipe/${id}`)
+    }
 
       let recipes = [];
       for (let i = 0; i < 20; i++) recipes.push(<Recipe />);
