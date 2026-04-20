74ef23d8765a7d6d8b07ea0ce9e1ae4e4c9f8392
Wyatt936 <wyattosburn13@yahoo.com>
2026-04-19 21:12:14 -0500

    updated script to seed users

--- setup.bat
+++ setup.bat
@@ -7,4 +7,8 @@ docker exec -i sous-chef-backend-1 node src/RecipeAutomation.js
 
 docker exec -i sous-chef-backend-1 node src/ScrapeImages.js
 
+docker compose cp Backend/db/user_seed.sql postgres:/user_seed.sql
+
+docker compose exec postgres psql -U SousChef -d appdb -f /user_seed.sql
+
 pause
