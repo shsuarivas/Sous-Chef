6faca2565171c11812a1b777ab8699f959c44c60
Tyler Byars <tylerabyars@gmail.com>
2026-04-19 19:08:08 -0500

    syntax cleanup (OCD)

--- Frontend/src/MainPage/pages/ExplorePage/ExplorePage.jsx
+++ Frontend/src/MainPage/pages/ExplorePage/ExplorePage.jsx
@@ -1,5 +1,5 @@
 import styles from './ExplorePage.module.scss'
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 //passing prop to a function
